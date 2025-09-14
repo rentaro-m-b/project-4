a54f815f0d2180745ba73b0ec470148d38cb601f
montre <113481566+rentaro-m-b@users.noreply.github.com>
2025-09-14 18:46:31 +0900

    feat: set up jooq

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     alias(libs.plugins.kotest)
     alias(libs.plugins.kotlin.plugin.serialization)
     alias(libs.plugins.flyway)
+    alias(libs.plugins.jooq.codegen)
 }
 
 group = "com.example"
@@ -25,7 +26,6 @@ dependencies {
     implementation(libs.ktor.server.core)
     implementation(libs.ktor.server.netty)
     implementation(libs.logback.classic)
-    implementation(libs.ktor.server.core)
     implementation(libs.ktor.server.config.yaml)
     implementation(libs.ktor.server.status.pages)
     implementation(libs.ktor.server.content.negotiation)
@@ -35,12 +35,15 @@ dependencies {
     implementation(libs.koin.ktor)
     implementation(libs.koin.logger.slf4j)
     implementation(libs.postgresql)
-    implementation(libs.flyway.database.postgresql)
+    implementation(libs.jooq)
     testImplementation(libs.ktor.server.test.host)
     testImplementation(libs.kotlin.test.junit)
     testImplementation(libs.kotest.framework.engine)
     testImplementation(libs.kotest.assertions.core)
     testImplementation(libs.rest.assured)
+    jooqCodegen(libs.postgresql)
+    jooqCodegen(libs.jooq.meta)
+    jooqCodegen(libs.jooq.codegen)
 }
 
 flyway {
@@ -48,4 +51,38 @@ flyway {
     user = "root"
     password = "password"
     locations = arrayOf("filesystem:src/main/resources/db/migration")
+}
+
+val codeGenDir = layout.buildDirectory.dir("generated/jooq")
+
+jooq {
+    configuration {
+        jdbc {
+            driver = "org.postgresql.Driver"
+            url = "jdbc:postgresql://localhost:54332/main"
+            user = "root"
+            password = "password"
+        }
+        generator {
+            name = "org.jooq.codegen.KotlinGenerator"
+            database {
+                name = "org.jooq.meta.postgres.PostgresDatabase"
+                inputSchema = "public"
+                includes = ".*"
+                excludes = "flyway_schema_history"
+            }
+            target {
+                packageName = "com.example.db"
+                directory = codeGenDir.get().asFile.path
+            }
+        }
+    }
+}
+
+sourceSets.main {
+    kotlin.srcDirs(codeGenDir)
+}
+
+tasks.named("compileKotlin") {
+    dependsOn("jooqCodegen")
 }
