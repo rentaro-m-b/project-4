0d3e8162131098036e76f8d497d9764bc6b9d837
montre <113481566+rentaro-m-b@users.noreply.github.com>
2025-09-27 22:53:39 +0900

    refactor: remove trashes

--- src/test/kotlin/task/TaskIntegrationTest.kt
+++ src/test/kotlin/task/TaskIntegrationTest.kt
@@ -3,17 +3,14 @@ package task
 import com.example.module
 import com.example.task.controller.CreateTaskRequest
 import io.kotest.core.spec.style.FunSpec
-import io.kotest.matchers.shouldBe
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.request.post
 import io.ktor.client.request.setBody
 import io.ktor.http.ContentType
-import io.ktor.http.HttpStatusCode.Companion.OK
 import io.ktor.http.contentType
 import io.ktor.serialization.kotlinx.json.json
 import io.ktor.server.config.yaml.YamlConfigLoader
 import io.ktor.server.testing.testApplication
-import io.netty.handler.codec.http.HttpHeaderNames.LOCATION
 import org.koin.core.context.stopKoin
 
 class TaskIntegrationTest :
