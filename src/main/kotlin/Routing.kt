432a35812256d4f525dc62740fc271fa128bbf19
montre <113481566+rentaro-m-b@users.noreply.github.com>
2025-11-26 23:03:18 +0900

    refactor: a few corrections

--- src/main/kotlin/Routing.kt
+++ src/main/kotlin/Routing.kt
@@ -1,7 +1,6 @@
 package com.example
 
 import com.example.controller.stickynote.stickyNoteRoutes
-import com.example.domain.stickynote.StickyNoteRepository
 import com.example.infra.stickynote.StickyNoteRepositoryImpl
 import com.example.usecase.stickynote.CreateStickyNoteUseCase
 import com.example.usecase.stickynote.ListStickyNotesUseCase
--- src/main/kotlin/domain/stickynote/StickyNote.kt
+++ src/main/kotlin/domain/stickynote/StickyNote.kt
@@ -1,7 +1,5 @@
 package com.example.domain.stickynote
 
-import kotlinx.serialization.Contextual
-import kotlinx.serialization.Serializable
 import java.time.LocalDateTime
 
 data class StickyNote(
