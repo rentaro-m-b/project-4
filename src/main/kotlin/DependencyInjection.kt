6fcc33ce3cb49094ee330f355710124265d83481
montre <113481566+rentaro-m-b@users.noreply.github.com>
2026-01-23 09:11:57 +0900

    fix: koin-annotations

--- src/main/kotlin/DependencyInjection.kt
+++ src/main/kotlin/DependencyInjection.kt
@@ -1,44 +1,22 @@
 package com.example
 
-import com.example.domain.stickynote.StickyNoteRepository
 import com.example.infra.DataSource
-import com.example.infra.stickynote.StickyNoteRepositoryImpl
-import com.example.usecase.stickynote.CreateStickyNoteUseCase
-import com.example.usecase.stickynote.DeleteStickyNoteUseCase
-import com.example.usecase.stickynote.IdGenerator
-import com.example.usecase.stickynote.ListStickyNotesUseCase
-import com.example.usecase.stickynote.UpdateStickyNoteUseCase
 import io.ktor.server.application.Application
 import io.ktor.server.application.install
 import org.jooq.SQLDialect
 import org.jooq.impl.DSL
-import org.koin.core.module.dsl.bind
-import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.includes
 import org.koin.dsl.module
 import org.koin.ksp.generated.koinConfiguration
 import org.koin.ktor.plugin.Koin
 
-// fun Application.configureDependencyInjection() {
-//    install(Koin) {
-//        includes(KoinApplication.koinConfiguration())
-//    }
-// }
-
 fun Application.configureDependencyInjection() {
     install(Koin) {
         modules(
-            listOf(
-                module {
-                    single { DSL.using(DataSource(environment.config).getConnection(), SQLDialect.POSTGRES) }
-                    singleOf(::StickyNoteRepositoryImpl) { bind<StickyNoteRepository>() }
-                    singleOf(::ListStickyNotesUseCase)
-                    singleOf(::CreateStickyNoteUseCase)
-                    singleOf(::UpdateStickyNoteUseCase)
-                    singleOf(::DeleteStickyNoteUseCase)
-                    singleOf(::IdGenerator)
-                },
-            ),
+            module {
+                single { DSL.using(DataSource(environment.config).getConnection(), SQLDialect.POSTGRES) }
+            },
         )
+        includes(KoinApplication.koinConfiguration())
     }
 }
